aa86021731a560272c2f2f32a48ac8990bc13fb9
Rakesh Kushwaha <rk7518329420@gmail.com>
2025-07-27 19:32:34 +0530

    commit

---
+++ Searching and sorting/BinarySearch.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let left=0;
+    let right=nums.length-1;
+
+    while(right>=left){
+        let middle= Math.floor((right+left)/2)
+
+        if(nums[middle]==target)
+        return middle;
+
+        else if(nums[middle]>target){
+       right=middle-1;
+        }
+        
+        else
+        left= middle+1;
+    }
+
+
+    return -1
+    
+};
