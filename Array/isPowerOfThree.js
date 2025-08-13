aac4f7480b6cd9bc01509d699a8744fc99314475
Rakesh Kushwaha <rk7518329420@gmail.com>
2025-08-13 22:09:09 +0530

    commit

---
+++ Array/isPowerOfThree.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+
+    // if (n <= 0) return false;
+    // let logVal = Math.log10(n) / Math.log10(3);
+    // return Number.isInteger(logVal);
+      return n > 0 && 1162261467 % n === 0;
+};
