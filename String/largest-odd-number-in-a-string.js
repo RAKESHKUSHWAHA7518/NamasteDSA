fa47f9fe12ef6932b4d1cc3e1d51a928f78ed929
Rakesh Kushwaha <rk7518329420@gmail.com>
2025-08-22 19:30:14 +0530

    commit solution

--- String/largest-odd-number-in-a-string.js
+++ String/largest-odd-number-in-a-string.js
@@ -20,3 +20,23 @@ var largestOddNumber = function(s) {
   return "";
     
 };
+
+
+
+/**
+ * @param {string} num
+ * @return {string}
+ */
+var largestOddNumber = function(s) {
+     
+
+     let n = s.length - 1;
+  while (n >= 0) {
+    if (Number(s[n]) % 2 == 1) {
+      return s.substring(0, n + 1);
+    }
+    --n;
+  }
+  return "";
+    
+};
