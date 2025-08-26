0e4c2f48661365aab936b68f438206cae2f37c02
Rakesh Kushwaha <rk7518329420@gmail.com>
2025-08-26 08:59:00 +0530

    queues operation

---
+++ stack and q/queues.js
@@ -0,0 +1,11 @@
+let queues = [];
+
+queues.push(1)
+queues.push(2)    
+queues.push(3)  
+queues.push(4)  
+queues.push(5)
+queues.shift()
+let front = queues[0]
+console.log(queues);
+console.log(front);
