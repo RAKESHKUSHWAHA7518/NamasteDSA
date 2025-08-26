2da01bbafb0d5d5a46106538a264e0c753878f45
Rakesh Kushwaha <rk7518329420@gmail.com>
2025-08-26 08:52:25 +0530

    stack operation

---
+++ stack and q/stack.js
@@ -0,0 +1,12 @@
+let stack =[];
+
+stack.push(1)
+stack.push(2)    
+stack.push(3)
+console.log(stack);
+console.log(stack.pop());
+stack.push(4)   
+stack.push(5)
+stack.push(6)
+console.log(stack);
+console.log(stack[stack.length-1]);
