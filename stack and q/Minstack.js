e32c6e78799bbdc5e1da898350dfedd165067be1
Rakesh Kushwaha <99179878+RAKESHKUSHWAHA7518@users.noreply.github.com>
2025-09-04 19:37:40 +0530

    minstack

---
+++ stack and q/Minstack.js
@@ -0,0 +1,47 @@
+
+var MinStack = function() {
+    this.s1=[]
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function(val) {
+    if(this.s1.length===0){
+        this.s1.push([val,val])
+    } else{
+        let minval=Math.min(val,this.s1[this.s1.length-1][1])
+        this.s1.push([val,minval])
+    }
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    this.s1.pop()
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    return this.s1[this.s1.length-1][0]
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    return this.s1[this.s1.length-1][1]
+};
+
+/** 
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
