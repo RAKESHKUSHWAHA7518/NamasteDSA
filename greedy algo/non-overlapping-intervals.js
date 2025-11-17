3d012c33f5d51897fe70cb052783c27d4701a7ce
Rakesh Kushwaha <99179878+RAKESHKUSHWAHA7518@users.noreply.github.com>
2025-11-17 12:09:56 +0530

    non-overlapping-intervals

---
+++ greedy algo/non-overlapping-intervals.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+var eraseOverlapIntervals = function(arr) {
+    arr.sort((a, b) => (a[1] - b[1]));
+    let removeCount = 0;
+    let k = -Infinity;
+    for(let i=0; i < arr.length; i++){
+        if(arr[i][0] < k){
+            ++removeCount;
+        } else {
+            k = arr[i][1];
+        }
+    }
+    return removeCount;
+};
