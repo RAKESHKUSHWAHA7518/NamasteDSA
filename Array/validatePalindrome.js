51a305be7b018bc20b053e631ec25b174741d6a9
Rakesh Kushwaha <99179878+RAKESHKUSHWAHA7518@users.noreply.github.com>
2026-04-24 23:08:52 +0530

    validatePalindrome

---
+++ Array/validatePalindrome.js
@@ -0,0 +1,12 @@
+function validatePalindrome(str) {
+    // Your implementation
+      const cleaned = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+
+    // Compare cleaned string with its reverse
+    return cleaned === cleaned.split('').reverse().join('');
+}
+
+//For the purpose of user debugging.
+validatePalindrome("race a car");
+
+module.exports = validatePalindrome
